ac63d612b31cea1e6ed8d1cfa31e3685617e067d
Dave <dave@wearecrystal.uk>
2019-07-09 14:23:06 +0100

    Basic webpack config, include bootstrap grid & reboot

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './app/scripts/main.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+
+  devServer: {
+    port: 3000,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.pug',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+  }
+  if (argv.mode === 'production') {
+  }
+
+  return config;
+};
