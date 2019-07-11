cf153c826a04feec60385ee13ea38c25d9195dc7
DaveP <dave@wearecrystal.uk>
2019-07-11 15:58:38 +0100

    Working generator

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ import javascript from 'highlight.js/lib/languages/javascript';
 import json from 'highlight.js/lib/languages/json';
 import 'highlight.js/styles/atom-one-dark.css';
 import hljs from 'highlight.js/lib/highlight';
+import cry from '../../api/cry';
 import twemoji from 'twemoji';
 import '../styles/main.scss';
 
@@ -12,4 +13,14 @@ hljs.initHighlightingOnLoad();
 twemoji.parse(document.getElementById('wrapper'), {
   folder: 'svg',
   ext: '.svg',
+});
+
+document.querySelector('textarea#tears').addEventListener('input', function() {
+  if (!this.value) return false;
+
+  cry(this.value)
+    .then(tears => {
+      document.querySelector('#out').innerText = tears;
+    })
+    .catch(e => console.log(e));
 });
