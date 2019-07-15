4b368a2b390f04aea9a1a7f215ae524b45240f1c
DaveP <dave@wearecrystal.uk>
2019-07-15 22:46:23 +0100

    Run generator on page load

--- app/scripts/generator.js
+++ app/scripts/generator.js
@@ -3,16 +3,22 @@ import cry from '../../api/cry';
 const input = document.querySelector('textarea#tears');
 const output = document.querySelector('#out');
 
-input.addEventListener('input', function() {
-  if (!this.value) return false;
+document.addEventListener(
+  'DOMContentLoaded',
+  () => tears(input.placeholder),
+  false
+);
 
-  cry(this.value)
-    .then(tears => {
-      output.innerText = tears;
-    })
-    .catch(e => console.log(e));
+input.addEventListener('input', function() {
+  if (this.value) tears(this.value);
 });
 
 output.addEventListener('click', () => {
   window.getSelection().selectAllChildren(output);
 });
+
+function tears(val) {
+  cry(val)
+    .then(tears => (output.innerText = tears))
+    .catch(e => console.log(e));
+}
