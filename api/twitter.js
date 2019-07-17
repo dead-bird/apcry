4cfd594e2062bb973ded7cf69e86a3be0b7edb80
DaveP <dave@wearecrystal.uk>
2019-07-17 23:17:16 +0100

    Make sendTweet return a Promise

--- api/twitter.js
+++ api/twitter.js
@@ -1,15 +1,24 @@
 import Twitter from 'twitter';
 import dotenv from 'dotenv';
+import log from './log';
 
 dotenv.config();
 
 const client = new Twitter(process.env);
 
-function tweet(status) {
-  client
-    .post('statuses/update', { status })
-    .then(tweet => {
-      console.log(`Tweet sent @ ${tweet.created_at}: ${status}`);
-    })
-    .catch(e => console.error(e));
-}
+const sendTweet = item => {
+  const status = item.tears;
+
+  return new Promise((resolve, reject) => {
+    client
+      .post('statuses/update', { status })
+      .then(tweet => {
+        log.info(`Tweet sent: ${status}`);
+
+        resolve();
+      })
+      .catch(e => reject(e));
+  });
+};
+
+export default { sendTweet };
