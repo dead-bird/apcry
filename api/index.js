886b6579871b945c87bda0069cbfa07bd7637a85
DaveP <hello@davep.co>
2020-12-10 15:24:02 +0000

    feat: added prettier config, run formatter

--- api/index.js
+++ api/index.js
@@ -11,17 +11,20 @@ app.use(bodyParser.json());
 
 // Set Express headers
 app.use((req, res, next) => {
-	// Allow requests from any origin
-	res.header('Access-Control-Allow-Origin', '*');
+  // Allow requests from any origin
+  res.header('Access-Control-Allow-Origin', '*');
 
-	// Set which HTTP headers can be used the request
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
+  // Set which HTTP headers can be used the request
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Authorization, Accept'
+  );
 
-	// Only allow GET/POST requests
-	res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST');
+  // Only allow GET/POST requests
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST');
 
-	// Onwards!
-	next();
+  // Onwards!
+  next();
 });
 
 // We're proxying apache to localhost so we wanna grab the client’s IP
@@ -30,35 +33,35 @@ app.set('trust proxy', 1);
 
 // Rate limit per IP address
 app.use(
-	limit({
-		message: 'alright there cowboy, time to stop 🤠',
-		windowMs: 15 * 60 * 1000, // 15 minutes
-		max: 100, // limit each IP to 100 requests per windowMs
-	})
+  limit({
+    message: 'alright there cowboy, time to stop 🤠',
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
 );
 
 // List items that are in the Tweet queue - only used internally
 app.get('/queue', (req, res) => {
-	queue
-		.list()
-		.then((items) => res.json({ items }))
-		.catch((e) => log.error(e));
+  queue
+    .list()
+    .then(items => res.json({ items }))
+    .catch(e => log.error(e));
 });
 
 // Make some tears
 app.post('/cry', ({ body }, res) => {
-	const { input } = body;
+  const { input } = body;
 
-	if (input.length > 2000) {
-		return res.status(413).send("oh noes, that's too much string my guy (╥_╥)");
-	}
+  if (input.length > 2000) {
+    return res.status(413).send("oh noes, that's too much string my guy (╥_╥)");
+  }
 
-	cry(input)
-		.then((tears) => {
-			queue.add({ input, tears });
-			res.json({ input, tears });
-		})
-		.catch((e) => res.status(e.status || 400).send(e));
+  cry(input)
+    .then(tears => {
+      queue.add({ input, tears });
+      res.json({ input, tears });
+    })
+    .catch(e => res.status(e.status || 400).send(e));
 });
 
 // Listen for connections
